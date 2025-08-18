93fa53ef9a3c5eaa28c692a2c4a9de41501b4015
dileepkakara <dileepkakara20@gmail.com>
2025-08-18 19:57:02 +0530

    your message

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,31 +1,47 @@
-import Header from '../Header'
+
 import CartContext from '../../context/CartContext'
+
+import Header from '../Header'
 import EmptyCartView from '../EmptyCartView'
 import CartListView from '../CartListView'
+import CartSummary from '../CartSummary'
 
 import './index.css'
 
 const Cart = () => (
   <CartContext.Consumer>
     {value => {
-      const {cartList} = value
-      const ShowEmptyView = cartList.length === 0
+      const { cartList, removeAllCartItems } = value
+      const showEmptyView = cartList.length === 0
+      const onClickRemoveAllBtn = () => {
+        removeAllCartItems()
+      }
+
       return (
         <>
           <Header />
-          {ShowEmptyView ? (
-            <EmptyCartView />
-          ) : (
-            <div className="cart-container">
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
               <div className="cart-content-container">
                 <h1 className="cart-heading">My Cart</h1>
+                <button
+                  type="button"
+                  className="remove-all-btn"
+                  onClick={onClickRemoveAllBtn}
+                >
+                  Remove All
+                </button>
                 <CartListView />
+                <CartSummary />
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </>
       )
     }}
   </CartContext.Consumer>
 )
+
 export default Cart
